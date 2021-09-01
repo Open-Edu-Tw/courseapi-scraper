b9c37fe72d54aceffb1505953ac9f8233bc22b6c
maureen.hsu@gmail.com <maureen.hsu@gmail.com>
2021-09-01 22:26:12 +0800

    add CourseItemCheckPipeline, SaveToCsvPipeline

--- src/ocw/ocw/pipelines.py
+++ src/ocw/ocw/pipelines.py
@@ -2,17 +2,42 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import os
 import datetime
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import pandas as pd
+from scrapy.exceptions import DropItem
 
 
-class CourseItemPipeline:
+class CourseItemCheckPipeline:
     def process_item(self, item, spider):
         # type check
         for colname in ["startDate", "endDate"]:
             if item[colname] and not isinstance(item[colname], datetime.datetime):
-                spider.logger.error(f"[TypeError] {colname} in course is not datetime")
+                spider.logger.error(f"[Type Error] {colname} in course {item['name']} is not datetime.")
+        
+        # mandatory column
+        for colname in spider.mandatory_columns:
+            if not item[colname]:
+                raise DropItem(f"[Empty Result] {colname} in course {item['name']} is empty.")
+            
         return item
+
+class SaveToCsvPipeline:
+    file = None
+    def open_spider(self, spider):
+        self.items = []
+    
+    def process_item(self, item, spider):
+        self.items.append(dict(item))
+        return item
+    
+    def close_spider(self, spider):
+        save_path = os.path.join(spider.settings["FILES_STORE"], spider.name, f"{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        
+        df = pd.DataFrame(self.items)
+        df[spider.defined_columns].to_csv(save_path)
+        spider.logger.info(f"Csv is exported to {save_path}. Total records={len(df)}")
+
+        
