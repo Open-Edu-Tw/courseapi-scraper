39a74185ff08b392b681886a3d3c5b6c8f123bc6
pan93412 <pan93412@gmail.com>
2021-11-20 12:04:04 +0800

    feat(settings): exit if there are 3 errors happened

--- ocw/settings.py
+++ ocw/settings.py
@@ -95,3 +95,5 @@ ITEM_PIPELINES = {
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+CLOSESPIDER_ERRORCOUNT = 3
