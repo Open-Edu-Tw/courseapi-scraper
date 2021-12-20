b6e40a4cd772b5ce5dff9d15b0c89eb6f93663da
maureen.hsu@gmail.com <maureen.hsu@gmail.com>
2021-12-20 22:57:40 +0800

    add MoocsSpider

---
+++ ocw/spiders/moocs.py
@@ -0,0 +1,50 @@
+from abc import ABC
+
+import scrapy
+from ocw.items import CourseItem
+from ocw.spiders.Scraper import OCWScraper
+
+url = "https://taiwanmooc.org/listing/ajaxfilter_pagelink"
+
+
+class MoocsSpider(OCWScraper):
+    name = 'moocs'
+    allowed_domains = ['taiwanmooc.org']
+
+    def start_requests(self):
+        yield scrapy.Request(url=url, callback=self.parse_course_directoy)
+
+    # def parse_category(self, response):
+    #     categories = response.xpath("//h2[contains(text(), '課程分類')]/ancestor::section[@class='section-courses']//a").extract()
+    #     for cateogry in categories:
+    #         href = cateogry.xpath("./@href").get()
+    #         scrapy.Request(url=response.urljoin(href), callback=self.parse_course_directoy)
+
+    def parse_course_directoy(self, response):
+        courses = response.xpath("//div[@class='course-box']")
+        for course in courses:
+            course_url = course.xpath(".//a/@href").get()
+            yield scrapy.Request(url=course_url,
+                                 callback=self.parse_course)
+
+        # next page
+        if "page" not in response.meta:  # run only at first page
+            last_page_num = int(response.xpath("//a[@data-ci-pagination-page and contains(text(), 'Last')]/@data-ci-pagination-page").get())
+            for p in range(1, last_page_num + 1):
+                yield scrapy.Request(f"{url}/{len(courses) * p}", callback=self.parse_course_directoy, meta={"page": p})
+
+    def parse_course(self, response):
+        course_item = CourseItem(name=response.xpath("//h1/text()").get(),
+                                 url=response.url,
+                                 instructor=response.xpath(
+                                     "//div[@class='teacher-box']//*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6 or self::h7 or self::h8]/text()").extract(),
+                                 provider_institution=response.xpath("//*[contains(text(), 'Institution')]/following-sibling::a/text()").get(),
+                                 description=self._get_description(response),
+                                 source="磨客師")
+        yield course_item
+
+    @OCWScraper.get_element_handler(default_return_value="")
+    def _get_description(self, response) -> str:
+        texts = response.xpath("//div[@id='cs-desc']/div[@class='content-box']/descendant-or-self::*/text()").extract()
+        description = " ".join(texts).strip().replace("\n", " ")
+        return description
